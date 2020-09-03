7b9943fc606587ec697dcc3e5ef3e9bed70fa06f
HaithamDesouky <haithamadesouky@gmail.com>
2020-09-03 11:42:50 +0100

    home page partially styled

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -15,9 +15,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo">
-          BandFinder<i className="fab fa-react"></i>
-        </h1>
+        <h1 className="navbar-logo">BANDFINDER</h1>
         <div className="menu-icon" onClick={this.handleClick}>
           <i
             className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
